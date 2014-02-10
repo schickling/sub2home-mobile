6191db4c99f383a4214d86aa7c12d0c2bdc1d85a
Johannes Schickling <schickling.j@gmail.com>
2014-02-10 09:43:38 +0100

    left width

--- app/js/app.js
+++ app/js/app.js
@@ -15,6 +15,7 @@ angular.module('mobile', [
       snapRemoteProvider.globalOptions = {
         disable: 'right',
         flickThreshold: 20,
+        maxPosition: 180,
       };
 
       $routeProvider.when('/', {

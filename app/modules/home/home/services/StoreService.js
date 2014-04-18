4d873ae2aa41e404c4b9af0e5209741f02afda14
Johannes Schickling <schickling.j@gmail.com>
2014-04-18 23:50:13 +0200

    store service

---
+++ app/modules/home/home/services/StoreService.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = function() {
+
+  return {
+
+    nextDeliveryTime: function(store, now) {
+      var deliveryTimes = store.deliveryTimesCollection,
+        day = now.getDay(),
+        minutes = now.getHours() * 60 + now.getMinutes();
+
+      deliveryTimes = deliveryTimes.sort((a, b) => a.startMinutes - b.startMinutes);
+
+      for (var i = 0; i < 7; i++) {
+        var filteredDeliveryTimes = deliveryTimes.filter(function(deliveryTime) {
+          return day === deliveryTime.dayOfWeek &&
+            ((i === 0 && deliveryTime.startMinutes >= minutes) || i !== 0);
+        });
+        if (filteredDeliveryTimes.length > 0) {
+          return filteredDeliveryTimes[0];
+        }
+        day = (day + 1) % 7;
+      }
+
+      return null;
+    },
+
+    isDelivering: function(store, now) {
+      if (!store.isOpen) {
+        return false;
+      }
+
+      var day = now.getDay(),
+        minutes = now.getHours() * 60 + now.getMinutes();
+
+      for (var i = 0; i < store.deliveryTimesCollection.length; i++) {
+        var deliveryTime = store.deliveryTimesCollection[i];
+        if (deliveryTime.dayOfWeek === day &&
+          deliveryTime.startMinutes <= minutes &&
+          deliveryTime.endMinutes >= minutes) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+
+  };
+
+};

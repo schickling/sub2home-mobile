15f76886f5f5ef58942b53b713ac7171925ae58d
Johannes Schickling <schickling.j@gmail.com>
2014-05-08 15:29:24 +0200

    Refactored postal input

--- app/modules/home/home/directives/postalInputDirective.js
+++ app/modules/home/home/directives/postalInputDirective.js
@@ -2,88 +2,88 @@
 
 var zipcoder = require('zipcoder');
 
-module.exports = [
+module.exports = ['$timeout',
 
-  function() {
+  function($timeout) {
     return {
       restrict: 'E',
       templateUrl: 'modules/home/home/directives/postalInputDirective.html',
       scope: true,
       link: function($scope, $elem, $attrs) {
+
         var input = $elem.find('input'),
-          compass = $elem.find('i'),
-          postal = '';
+          compass = $elem.find('i');
 
         $scope.isFocused = false;
         $scope.isLoading = false;
 
-        listenToInput();
-        listenToCompass();
-        watchParentScope();
-
-        function listenToInput() {
-          input.on('focus', function() {
-            $scope.$parent[$attrs.appFocused] = true;
-            $scope.isFocused = true;
-            $scope.$parent.$apply();
-          });
-
-          input.on('blur', function() {
-            $scope.$parent[$attrs.appFocused] = false;
-            $scope.isFocused = false;
-            $scope.$parent.$apply();
-          });
-
-          input.on('keydown', function(e) {
-
-            if (e.keyCode === 8) {
-              return;
-            }
-
-            if (input.val().length >= 5) {
-              e.preventDefault();
-              return;
-            }
-
-            if (e.keyCode > 31 && (e.keyCode < 48 || e.keyCode > 57)) {
-              e.preventDefault();
-              return;
-            }
-          });
-
-          input.on('keyup', function() {
-            postal = input.val();
-            if (postal.length === 5) {
-              $scope.$parent[$attrs.appPostal] = postal;
-              $scope.$parent.$apply();
-              input[0].blur();
-            }
-          });
-        }
-
-        function listenToCompass() {
-          compass.on('click', function() {
-            determineLocation();
-          });
-        }
-
-        function watchParentScope() {
-          $scope.$parent.$watch($attrs.appDistrict, function() {
-            $scope.district = $scope.$parent[$attrs.appDistrict];
-          });
-        }
-
-        function determineLocation() {
+        $scope.determineLocation = function() {
           $scope.isLoading = true;
-          $scope.$apply();
           zipcoder.location(function(result) {
             $scope.isLoading = false;
-            $scope.$apply();
             input.val(result.zipcode);
+            $elem.addClass('postalOnly');
             $scope.$parent[$attrs.appPostal] = result.zipcode;
             $scope.$parent.$apply();
           });
-        }
+        };
+
+        $scope.blur = function() {
+          $scope.$parent[$attrs.appFocused] = false;
+          $scope.isFocused = false;
+        };
+
+        $scope.focus = function() {
+          $scope.$parent[$attrs.appFocused] = true;
+          $scope.isFocused = true;
+        };
+
+        $scope.keydown = function($event) {
+
+          // allow backspace
+          if ($event.keyCode === 8) {
+            return;
+          }
+
+          if (input.val().length >= 5) {
+            $event.preventDefault();
+            return;
+          }
+
+          // prevent everything but numbers
+          if ($event.keyCode > 31 && ($event.keyCode < 48 || $event.keyCode > 57)) {
+            $event.preventDefault();
+            return;
+          }
+
+        };
+
+        $scope.keyup = function() {
+
+          var postal = input.val();
+
+          if (postal.length === 5) {
+
+            $scope.$parent[$attrs.appPostal] = postal;
+
+            $elem.addClass('postalOnly');
+
+            // timeout hack needed for $apply cycle
+            $timeout(function() {
+              input[0].blur();
+            }, 0, false);
+
+          } else {
+            $elem.removeClass('postalOnly');
+          }
+
+        };
+
+        $scope.determineLocation();
+
+        $scope.$parent.$watch($attrs.appDistrict, function() {
+          $scope.district = $scope.$parent[$attrs.appDistrict];
+        });
 
       }
     }

28b92727fd1c45dab531c10392b9043f0fca5d41
Johannes Schickling <schickling.j@gmail.com>
2014-06-11 22:56:56 +0200

    back button

--- app/modules/store/selection/controllers/MainCtrl.js
+++ app/modules/store/selection/controllers/MainCtrl.js
@@ -1,11 +1,15 @@
 'use strict';
 
-module.exports = ['$scope', 'orderedItemModel',
+module.exports = ['$scope', 'orderedItemModel', '$window',
 
-  function($scope, orderedItemModel) {
+  function($scope, orderedItemModel, $window) {
 
     $scope.title = 'Sandwich';
     $scope.ingredientsCollection = orderedItemModel.orderedArticlesCollection[0].articleModel.ingredientCategoriesCollection[0].ingredientsCollection;
+
+    $scope.back = function() {
+      $window.history.back();
+    };
 
   }
 ];

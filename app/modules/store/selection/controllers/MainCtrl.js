513f1783b6e693091b5f3a9d94822ca624a20878
Johannes Schickling <schickling.j@gmail.com>
2014-06-12 13:15:19 +0200

    select ingredient

--- app/modules/store/selection/controllers/MainCtrl.js
+++ app/modules/store/selection/controllers/MainCtrl.js
@@ -5,10 +5,38 @@ module.exports = ['$scope', 'orderedItemModel', '$window',
   function($scope, orderedItemModel, $window) {
 
     $scope.title = 'Sandwich';
-    $scope.ingredientsCollection = orderedItemModel.orderedArticlesCollection[0].articleModel.ingredientCategoriesCollection[0].ingredientsCollection;
+    $scope.ingredientCategoryModel = orderedItemModel.orderedArticlesCollection[0].articleModel.ingredientCategoriesCollection[0];
+    $scope.ingredientsCollection = $scope.ingredientCategoryModel.ingredientsCollection;
+    $scope.type = 'ingredient';
 
     $scope.back = function() {
       $window.history.back();
+    };
+
+    $scope.selectIngredient = function(ingredientModel) {
+
+      var newIsSelected = !ingredientModel.isSelected;
+
+      var numberOfSelectedIngredients = $scope.ingredientsCollection.reduce(function(sum, model) {
+        return sum + model.isSelected;
+      }, 0);
+
+      if ($scope.ingredientCategoryModel.isMandatory && numberOfSelectedIngredients <= 1 && !newIsSelected) {
+        return;
+      }
+
+      if ($scope.ingredientCategoryModel.isSingle && numberOfSelectedIngredients > 0) {
+        $scope.ingredientsCollection.forEach(function(model) {
+          model.isSelected = false;
+        });
+      }
+
+      ingredientModel.isSelected = newIsSelected;
+
+    };
+
+    $scope.next = function() {
+
     };
 
   }

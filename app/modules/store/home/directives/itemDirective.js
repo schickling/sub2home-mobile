1f97856fabc12958c5501e400214fc6ca72508ec
Johannes Schickling <schickling.j@gmail.com>
2014-06-17 10:20:35 +0200

    slideToggle .itemDescription - closes #23

--- app/modules/store/home/directives/itemDirective.js
+++ app/modules/store/home/directives/itemDirective.js
@@ -8,6 +8,8 @@ module.exports = ['RoutingService',
       templateUrl: 'modules/store/home/directives/itemDirective.html',
       link: function($scope, $elem, $attrs) {
 
+        $scope.hidden = true;
+
         $scope.getBuyClass = function(icon) {
           return 'buy' + icon.substring(0, 1).toUpperCase() + icon.substring(1);
         };

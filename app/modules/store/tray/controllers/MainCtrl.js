85474c858b698ace84a59e5e8ac04c1fd50fd778
tenthe <philipp.zehnder@gmx.de>
2014-08-18 11:55:01 +0200

    Warning when minimum value not reached works now

--- app/modules/store/tray/controllers/MainCtrl.js
+++ app/modules/store/tray/controllers/MainCtrl.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = ['$scope', 'TrayStorageService', 'TrayService',
+module.exports = ['$scope', 'TrayStorageService', 'TrayService', 'PersistenceService',
 
-  function($scope, TrayStorageService, TrayService) {
+  function($scope, TrayStorageService, TrayService, PersistenceService) {
     $scope.allSingleItems = TrayStorageService.getAllSingleItems();
 
     $scope.allSubItems = TrayStorageService.getAllSubItems();
@@ -10,5 +10,8 @@ module.exports = ['$scope', 'TrayStorageService', 'TrayService',
     $scope.allMenuItems = TrayStorageService.getAllMenuItems();
 
     $scope.totalAmount = TrayService.getTotalAmount();
+
+    $scope.deliveryAreaModel = PersistenceService.load('selectedDeliveryAreaModel');
+
   }
 ];

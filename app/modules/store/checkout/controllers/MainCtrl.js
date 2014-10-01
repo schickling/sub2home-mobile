160c17ce3106607470d19bc1e3ba4544d8a80214
Philipp Zehnder <philipp.zehnder@gmx.de>
2014-10-01 22:32:20 +0200

    Code rebasing at the checkout site

--- app/modules/store/checkout/controllers/MainCtrl.js
+++ app/modules/store/checkout/controllers/MainCtrl.js
@@ -1,40 +1,43 @@
 'use strict';
 
-module.exports = [ '$scope', 'PersistenceService', '$timeout',
+module.exports = ['$scope', 'PersistenceService', '$timeout',
 
   function($scope, PersistenceService, $timeout) {
 
-        var getTimeToDelivery = function(deliveryTime) {
-          var tmpTime = new Date();
-          var restTime = deliveryTime - (tmpTime.getHours() * 60);
-          restTime = restTime - (tmpTime.getMinutes());
+    var getTimeToDelivery = function(deliveryTime) {
+      var tmpTime = new Date();
+      var restTime = deliveryTime - (tmpTime.getHours() * 60);
+      restTime = restTime - (tmpTime.getMinutes());
 
-          return restTime;
-        };
+      return restTime;
+    };
 
-        $scope.userInfo = PersistenceService.load('formData');
-        $scope.storeModel = PersistenceService.load('storeModel');
-
-
-        $scope.restTime = getTimeToDelivery(PersistenceService.load('timeToDelivery'));
+    $scope.userInfo = PersistenceService.load('formData');
+    $scope.storeModel = PersistenceService.load('storeModel');
 
 
+    $scope.restTime = getTimeToDelivery(PersistenceService.load('timeToDelivery'));
 
-        var countDown = function () {
-          $timeout(function() {
-            var restTime = getTimeToDelivery(PersistenceService.load('timeToDelivery'));
-            if (restTime > 0) {
-                if (restTime % 5 === 0) {
-                  $scope.restTime = restTime;
-                  $scope.$apply();
-                }
-                countDown();
-            }
-          }, 60000);
 
-        };
 
-        countDown();
+    var countDown = function() {
+      var restTime = getTimeToDelivery(PersistenceService.load('timeToDelivery'));
+      if (restTime > 0) {
+        if (restTime % 5 === 0) {
+          $scope.restTime = restTime;
+          $scope.$apply();
+        }
+
+        $timeout(function() {
+          countDown();
+        }, 60000);
+      } else {
+          $scope.restTime = 0;
+      }
+
+    };
+
+    countDown();
 
   }
 ];

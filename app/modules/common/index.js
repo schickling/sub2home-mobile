5d858254682c47126210e1a11f6d8c413459a241
Johannes Schickling <schickling.j@gmail.com>
2014-10-01 14:30:38 +0200

    image lazy load - closes #125

--- app/modules/common/index.js
+++ app/modules/common/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var compassDirective = require('./directives/compassDirective');
+var lazyImageDirective = require('./directives/lazyImageDirective');
 var StringUtilService = require('./services/StringUtilService');
 var PostalOracleService = require('./services/PostalOracleService');
 var PersistenceService = require('./services/PersistenceService');
@@ -12,6 +13,7 @@ var NumberFixedLength = require('./filters/numberFixedLength');
 
 module.exports = angular.module('common', [])
   .directive('compass', compassDirective)
+  .directive('lazyImage', lazyImageDirective)
   .service('StringUtilService', StringUtilService)
   .service('PostalOracleService', PostalOracleService)
   .service('PersistenceService', PersistenceService)

158fd9af9b4e93406f24cef04509b639904e0348
Johannes Schickling <schickling.j@gmail.com>
2014-10-22 11:10:14 +0200

    environment service

--- app/modules/vendor/index.js
+++ app/modules/vendor/index.js
@@ -1,3 +1,19 @@
 'use strict';
 
-require('./segment');
+module.exports = angular.module('vendor', []);
+
+angular.module('vendor').run(['EnvironmentService', '$rootScope',
+  function(EnvironmentService, $rootScope) {
+
+    if (EnvironmentService.isProduction) {
+
+      require('./segment');
+
+      $rootScope.$on('$routeChangeSuccess', function() {
+        $window.analytics.page();
+      });
+
+    }
+
+  }
+]);

7fc7afd098250e9c28c0d28b461d640e2e6cb884
Philipp Zehnder <philipp.zehnder@gmx.de>
2014-12-28 21:20:25 +0100

    Fixed #109

--- app/modules/store/home/directives/itemDirective.js
+++ app/modules/store/home/directives/itemDirective.js
@@ -6,7 +6,7 @@ module.exports = ['RoutingService', 'TrayStorageService', 'NotificationService',
     return {
       restrict: 'E',
       templateUrl: 'modules/store/home/directives/itemDirective.html',
-      link: function($scope, $elem, $attrs) {
+      link: function($scope) {
 
         $scope.hidden = true;
 
@@ -14,11 +14,22 @@ module.exports = ['RoutingService', 'TrayStorageService', 'NotificationService',
           return 'buy' + icon.substring(0, 1).toUpperCase() + icon.substring(1);
         };
 
+        $scope.handleImageBuyClick = function(itemModel) {
+          if (itemModel.chainedArticlesCollection &&
+            itemModel.chainedArticlesCollection.length > 0) {
+            return;
+          } else {
+            $scope.handleBuyClick(itemModel);
+          }
+
+        };
+
         $scope.handleBuyClick = function(itemModel) {
 
           if (itemModel.hasOwnProperty('allowsIngredients')) {
             if (itemModel.allowsIngredients) {
-              RoutingService.navigate(':storeAlias/theke/artikel/' + itemModel.id);
+              RoutingService.navigate(':storeAlias/theke/artikel/' +
+                itemModel.id);
             } else {
               TrayStorageService.saveSingleItem(itemModel);
               NotificationService.setTrayNotification(itemModel.title);
